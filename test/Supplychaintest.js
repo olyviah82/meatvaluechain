58f0df15b9c34cc66e0075cb82bb6a315f3c3c36
olyviah82 <olyviah.amwoma@strathmore.edu>
2022-11-19 00:00:17 +0300

    UI changes

---
+++ test/Supplychaintest.js
@@ -0,0 +1,6 @@
+const SupplyChain= artifacts.require("../contracts/SupplyChain.sol");
+const Structure= artifacts.require("../contracts/Structure.sol");
+
+contract("SupplyChain",function (accounts){
+   
+})
--- truffle-config.js
+++ truffle-config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "client2/src/contracts"),
   networks: {
     develop: {
       host: "127.0.0.1",
