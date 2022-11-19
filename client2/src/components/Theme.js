462d4500a32a2ab355cd1944f0eea33e0ae21f86
olyviah82 <olyviah.amwoma@strathmore.edu>
2022-11-19 11:38:48 +0300

    update UI

---
+++ client2/src/components/Theme.js
@@ -0,0 +1,15 @@
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#7e1a22",
+      },
+      secondary: {
+        main: "#7e1a22",
+      },
+
+    },
+  });
+  
+  export default theme;
